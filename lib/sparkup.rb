4f8e45ddc3401ea677ddaeb920f3dcacb5f8e98e
Pockata <f1r3fl3x@gmail.com>
2011-05-09 01:09:48 +0300

    Added Jython fallback if Python is not natively installed

--- lib/sparkup.rb
+++ lib/sparkup.rb
@@ -1,13 +1,26 @@
 module Redcar
 
     class Sparkup
+
+        @@sparkup_cmd = "sparkup/sparkup --post-tag-guides"
+
         def initialize
 
-			self.menus
-			self.keymaps
+            self.menus
+            self.keymaps
+
+            unless self.hasPython
+                @@sparkup_cmd = "java -jar jython/jython.jar sparkup/sparkup"
+            end
         end
 
-         # Shows the menu in the toolbar
+        # Get Sparkup's cmd
+        def self.getCmd
+
+            @@sparkup_cmd
+        end
+
+        # Shows the menu in the toolbar
         def self.menus
             Redcar::Menu::Builder.build do
                 sub_menu "Plugins" do
@@ -27,8 +40,23 @@ module Redcar
             [map]
         end
 
-		# Open Sparkup's file (this) for edit
-		class EditSparkup < Redcar::Command
+        # Check if Python is installed
+        def self.hasPython
+
+            if Redcar.platform == :windows
+
+                # I haven't found a useable python test
+                # Got any? Send them to me pls
+                return false
+            else
+
+                pTest = `which python`
+                return pTest != ''
+            end
+        end
+
+        # Open Sparkup's file (this) for edit
+        class EditSparkup < Redcar::Command
             def execute
 
                 Project::Manager.open_project_for_path(
@@ -43,16 +71,21 @@ module Redcar
                 tab.edit_view.reset_undo
                 tab.focus
             end
-		end
+        end
 
         # Run Sparkup
-		class SparkupLine < Redcar::Command
+        class SparkupLine < EditTabCommand
             def execute
-                document = win.focussed_notebook.focussed_tab.document
-                document.replace_line(document.cursor_line) do |ltext|
-                    `cd #{Redcar.user_dir}/plugins/sparkup/assets && echo '#{ltext}' | sparkup/sparkup --post-tag-guides`
+                doc.replace_line(doc.cursor_line) do |ltext|
+
+                    cmd = Sparkup.getCmd
+                    dir = "#{Redcar.user_dir}/plugins/sparkup/assets"
+
+                    resp = `cd #{dir} && echo '#{ltext}' | #{cmd}`
+                    doc.cursor_offset = doc.cursor_line_start_offset
+                    resp
                 end
             end
-		end
+        end
     end
 end
